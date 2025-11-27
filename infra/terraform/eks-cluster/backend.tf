2a2c8107004f6fc45553463f1d36d097cf9dd8bd
RiddheshRameshSutar <riddheshrameshsutar@gmail.com>
2025-11-27 07:26:22 +0000

    CI CD 11

--- infra/terraform/eks-cluster/backend.tf
+++ infra/terraform/eks-cluster/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "todo-tfstate-e2fcbe"
+    bucket         = "todo-tfstate-5a42f3"
     key            = "eks-cluster/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "todo-tf-lock"
